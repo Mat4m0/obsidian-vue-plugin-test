26483465d2bbfff98685c29013ce53ff386e82b4
Mat4m0 <matthias.amon@me.com>
2024-02-18 11:35:17 +0100

    wip

--- src/plugin/main.ts
+++ src/plugin/main.ts
@@ -1,7 +1,10 @@
 import { Plugin } from 'obsidian'
 import { VIEW_TYPE_VUE_TEST, VueTestView } from './view'
+import { Pool } from 'pg'; 
 
 export default class VueTestPlugin extends Plugin {
+  dbPool: Pool | null = null;
+
   async onload() {
     this.registerView(VIEW_TYPE_VUE_TEST, leaf => new VueTestView(leaf))
 
@@ -14,10 +17,23 @@ export default class VueTestPlugin extends Plugin {
     })
 
     await this.createView()
+
+    this.dbPool = new Pool({
+      connectionString: 'postgres://jrsljuby:RxdHL-JgOE3FbaMqYm-2mONIhKrpLFWF@mouse.db.elephantsql.com/jrsljuby',
+    });
+
+    if (this.dbPool) {
+      await this.addSampleData();
+      await this.querySampleData();
+    }
+
   }
 
   async onunload() {
     this.findView()?.detach()
+    if (this.dbPool) {
+    await this.dbPool.end();
+    }
   }
 
   findView() {
@@ -39,5 +55,31 @@ export default class VueTestPlugin extends Plugin {
     const leaf = this.findView() ?? (await this.createView())
 
     this.app.workspace.revealLeaf(leaf)
+  }
+
+  async addSampleData() {
+    const text = 'INSERT INTO your_table(name, value) VALUES($1, $2)';
+    const values = ['test', '123'];
+
+    try {
+      //@ts-ignore
+      await this.dbPool.query(text, values);
+      console.log('Sample data added');
+    } catch (err) {
+      console.error('Error adding sample data', err);
+    }
+  }
+
+  // Example method to query data
+  async querySampleData() {
+    const text = 'SELECT * FROM your_table';
+
+    try {
+      //@ts-ignore
+      const res = await this.dbPool.query(text);
+      console.log('Query results:', res.rows);
+    } catch (err) {
+      console.error('Error querying data', err);
+    }
   }
 }
